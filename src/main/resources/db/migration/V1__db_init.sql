116eed9612303a5df5af054eca13696e5182d49c
odin0960 <121524275+odin0960@users.noreply.github.com>
2023-10-07 21:41:43 +0300

    fix DB

--- src/main/resources/db/migration/V1__db_init.sql
+++ src/main/resources/db/migration/V1__db_init.sql
@@ -1,3 +1,10 @@
+create TABLE IF NOT EXISTS users (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	username VARCHAR (200) NOT NULL UNIQUE CHECK (CHAR_LENGTH(username) >= 3),
+	password VARCHAR (2000) NOT NULL CHECK (CHAR_LENGTH(password) >= 8),
+	role VARCHAR (5) NOT NULL DEFAULT 'USER'
+);
+
 create TABLE IF NOT EXISTS links (
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
 	long_link VARCHAR (2000) NOT NULL,
@@ -6,17 +13,10 @@ create TABLE IF NOT EXISTS links (
     expire_date TIMESTAMP NOT NULL,
     user_id BIGINT NOT NULL,
 	count BIGINT,
-	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-create TABLE IF NOT EXISTS users (
-	id BIGINT PRIMARY KEY AUTO_INCREMENT,
-	username VARCHAR (200) NOT NULL UNIQUE CHECK (CHAR_LENGTH(username) >= 3),
-	password VARCHAR (2000) NOT NULL CHECK (CHAR_LENGTH(password) >= 8),
-	role VARCHAR (5) NOT NULL DEFAULT 'USER'
-);
-
-INSERT INTO users (username, pass, role)
+INSERT INTO users (username, password, role)
 VALUES
 	('admin',
 	'$2a$12$..wl30OG0nSU.2zLp7mxaeFxbuVySaIgdvpfQinWunXHGIRMWbZd2', --admin
