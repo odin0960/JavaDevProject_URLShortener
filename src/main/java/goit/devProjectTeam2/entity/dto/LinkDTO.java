e851abc789f53a70781cb598c5ff0964efe81c8b
olha <oh.semenovych@gmail.com>
2023-10-07 22:59:39 +0300

    Task1: add User and Link dto

--- src/main/java/goit/devProjectTeam2/entity/dto/LinkDTO.java
+++ src/main/java/goit/devProjectTeam2/entity/dto/LinkDTO.java
@@ -1,4 +1,35 @@
 package goit.devProjectTeam2.entity.dto;
 
+import goit.devProjectTeam2.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.time.ZonedDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "links")
 public class LinkDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String longLink;
+    private String token;
+    private Timestamp createDate;
+    private ZonedDateTime expireDate;
+    @ManyToOne
+    @JoinColumn(name = "id", nullable = false)
+    private UserDTO userId;
+    private Integer count;
+
 }
--- src/main/resources/db/migration/V1__db_init.sql
+++ src/main/resources/db/migration/V1__db_init.sql
@@ -1,27 +1,27 @@
-create TABLE IF NOT EXISTS users (
-	id BIGINT PRIMARY KEY AUTO_INCREMENT,
-	username VARCHAR (200) NOT NULL UNIQUE CHECK (CHAR_LENGTH(username) >= 3),
-	password VARCHAR (2000) NOT NULL CHECK (CHAR_LENGTH(password) >= 8),
-	role VARCHAR (5) NOT NULL DEFAULT 'USER'
+create TABLE IF NOT EXISTS users
+(
+    id       BIGINT  PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(200)  NOT NULL UNIQUE CHECK (CHAR_LENGTH(username) >= 3),
+    password VARCHAR(2000) NOT NULL CHECK (CHAR_LENGTH(password) >= 8),
+    role     VARCHAR(5)    NOT NULL DEFAULT 'USER'
 );
 
-create TABLE IF NOT EXISTS links (
-	id BIGINT PRIMARY KEY AUTO_INCREMENT,
-	long_link VARCHAR (2000) NOT NULL,
-	token VARCHAR (8) NOT NULL UNIQUE,
-	create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    expire_date TIMESTAMP NOT NULL,
-    user_id BIGINT NOT NULL,
-	count BIGINT,
-	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+create TABLE IF NOT EXISTS links
+(
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    long_link   VARCHAR(2000) NOT NULL,
+    token       VARCHAR(8)    NOT NULL UNIQUE,
+    create_date TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expire_date TIMESTAMP     NOT NULL,
+    user_id     BIGINT        NOT NULL,
+    count       BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 INSERT INTO users (username, password, role)
-VALUES
-	('admin',
-	'$2a$12$..wl30OG0nSU.2zLp7mxaeFxbuVySaIgdvpfQinWunXHGIRMWbZd2', --admin
-	 'ADMIN'
-	);
+VALUES ('admin',
+        '$2a$12$..wl30OG0nSU.2zLp7mxaeFxbuVySaIgdvpfQinWunXHGIRMWbZd2', - -admin
+            'ADMIN');
 
 
 
