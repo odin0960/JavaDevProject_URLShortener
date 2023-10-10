10796c8a07ec41fa8251100ac016dbc117b4c2b8
dvoinishnikova <dvonastya08@gmail.com>
2023-10-10 20:22:31 +0300

    fixing

--- src/main/java/goit/devProjectTeam2/link/LinkValidator.java
+++ src/main/java/goit/devProjectTeam2/link/LinkValidator.java
@@ -6,29 +6,17 @@ import java.net.URL;
 
 public class LinkValidator {
     public static void checkURL() {
-        String urlToCheck = "https://example.com";
-
+        String urlToCheck = "";
         try {
-            // Створюємо об'єкт URL на основі рядка URL
             URL url = new URL(urlToCheck);
-
-            // Відкриваємо з'єднання з сервером
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-            // Встановлюємо метод запиту на GET
             connection.setRequestMethod("GET");
-
-            // Отримуємо код статусу відповіді
             int statusCode = connection.getResponseCode();
-
-            // Перевіряємо, чи код статусу рівний 200
             if (statusCode == 200) {
                 System.out.println("Посилання " + urlToCheck + " повертає код статусу 200 (OK).");
             } else {
                 System.out.println("Посилання " + urlToCheck + " повертає код статусу " + statusCode);
             }
-
-            // Закриваємо з'єднання
             connection.disconnect();
         } catch (IOException e) {
             System.err.println("Виникла помилка: " + e.getMessage());
