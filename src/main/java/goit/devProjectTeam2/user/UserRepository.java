de4b407c5b1709fb5eda2b01f2cb16bbf612fc49
odin0960 <121524275+odin0960@users.noreply.github.com>
2023-10-14 19:57:08 +0300

    UserRepository - findByUsername

--- src/main/java/goit/devProjectTeam2/user/UserRepository.java
+++ src/main/java/goit/devProjectTeam2/user/UserRepository.java
@@ -1,4 +1,13 @@
 package goit.devProjectTeam2.user;
 
-public interface UserRepository {
+import goit.devProjectTeam2.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+//    @Query("from Users u where lower(u.username) like lower(:username)")
+//    User findByUsername(@Param("username") String username);
+    Optional<User>findByUsername findByUsername(String username);
 }
