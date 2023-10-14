f168fa100d8a3314fe1799174b4d9e825a980a4d
LubovLisenko <lubovlisenko1985@gmail.com>
2023-10-14 11:00:16 +0200

    user fix

--- src/main/java/goit/devProjectTeam2/user/UserRepository.java
+++ src/main/java/goit/devProjectTeam2/user/UserRepository.java
@@ -2,9 +2,25 @@ package goit.devProjectTeam2.user;
 
 import goit.devProjectTeam2.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+  //  User findByEmail(String email);
+    @Query(nativeQuery = true, value =
+            "SELECT username\n" +
+                    "FROM \"user\"\n" +
+                    "WHERE lower(username) LIKE lower(:query)")
+    List<String> searchUsername(@Param("query") String query);
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
--- src/main/java/goit/devProjectTeam2/user/UserService.java
+++ src/main/java/goit/devProjectTeam2/user/UserService.java
@@ -3,6 +3,8 @@ package goit.devProjectTeam2.user;
 import goit.devProjectTeam2.entity.User;
 import goit.devProjectTeam2.entity.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +13,19 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-public class UserService implements UserServiceInterface{
+public class UserService {
     private final UserRepository userRepository;
+    private final SessionFactory sessionFactory;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
     private PasswordEncoder passwordEncoder;
+    private User user;
+    private UserDTO userDTO;
 
-    @Override
-    public void saveUser(UserDTO userDTO) {
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void save(UserDTO userDTO) {
         User user = new User();
         user.setUsername(userDTO.getUsername());
         user.setEmail(userDTO.getEmail());
@@ -26,24 +35,20 @@ public class UserService implements UserServiceInterface{
             throw new IllegalArgumentException("User already exist with given username:" + user.getUsername());
         }
 
-            if    (user.getPassword().matches("^\"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]){8,50}$\"")){
-                user.setPassword(passwordEncoder.encode(user.getPassword()));
-
-            }
-        userRepository.save(user);
+        if (user.getPassword().matches("^\"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]){8,50}$\"")) {
+            user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+            user.setPassword(userDTO.getPassword());
+//
+//            }
+            System.out.println(userRepository.save(user));
+        }
     }
 
-    public List<User> listAll(){
-        return userRepository.findAll();
-    }
-    public User getById(Long id){
-        return   userRepository.findById(id)
-                .orElseThrow(()-> new IllegalArgumentException("User id not found"));
-    }
-
-    @Override
-    public User findUserByEmail(String email) {
-        return userRepository.findByEmail(email);
+    public boolean exists (String username, String email) {
+        if ((username == null)  | (email == null)){
+            return false;
+        }
+      return true;
     }
 
     private UserDTO mapToUserDto(User user){
@@ -54,4 +59,3 @@ public class UserService implements UserServiceInterface{
     }
 
 }
-
